fa9ffd62e0b4508e090c9bb53e3f9860c98f5552
Getaneh Biratu <mrgetaneh@gmail.com>
2024-07-18 00:04:56 +0000

    create a Question class to store answer  and question

--- run.py
+++ run.py
@@ -1,3 +1,11 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+
+
+class Question:
+    def __init__(self, prompt, option, answer)
+        self.prompt = prompt
+        self.option = option
+        self.answer = answer
+
+ def run_quiz(Question):
+           
